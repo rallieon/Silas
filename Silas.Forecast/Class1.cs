2231fd950c2888cfd71ec054c1d2df9109085266
Keith La Force <klaforce@skookum.com>
2013-05-04 11:29:59 -0400

    Setting up project architecture

---
+++ Silas.Forecast/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silas.Forecast
+{
+    public class Class1
+    {
+    }
+}

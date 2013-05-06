e081536695f2cc4c2db3749458a5da31640077bf
Keith La Force <klaforce@skookum.com>
2013-05-06 18:40:55 -0400

    Removing all user interface from API.  Added controllers for two different entities.

---
+++ Silas.API/Controllers/MutableDataEntryController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Silas.API.Models;
+
+namespace Silas.API.Controllers
+{
+    public class MutableDataEntryController : ApiController
+    {
+        private DataEntryEmptyContext db = new DataEntryEmptyContext();
+
+        // GET api/MutableDataEntry
+        public IEnumerable<DataEntry> GetDataEntries()
+        {
+            return db.Entries.AsEnumerable();
+        }
+
+        // GET api/MutableDataEntry/5
+        public DataEntry GetDataEntry(int id)
+        {
+            DataEntry dataentry = db.Entries.Find(id);
+            if (dataentry == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return dataentry;
+        }
+
+        // PUT api/MutableDataEntry/5
+        public HttpResponseMessage PutDataEntry(int id, DataEntry dataentry)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != dataentry.Id)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(dataentry).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/MutableDataEntry
+        public HttpResponseMessage PostDataEntry(DataEntry dataentry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entries.Add(dataentry);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, dataentry);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = dataentry.Id }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/MutableDataEntry/5
+        public HttpResponseMessage DeleteDataEntry(int id)
+        {
+            DataEntry dataentry = db.Entries.Find(id);
+            if (dataentry == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Entries.Remove(dataentry);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, dataentry);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
--- Silas.API/Global.asax.cs
+++ Silas.API/Global.asax.cs
@@ -18,12 +18,9 @@ namespace Silas.API
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             Database.SetInitializer(new DataEntryContextInitializer());
             using (var context = new DataEntryContext())

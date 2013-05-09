81277e90fe076cead66f8896ca09d83d068a8866
Keith La Force <klaforce@skookum.com>
2013-05-09 09:04:22 -0400

    Fixing a bug and adding clients for the tickers to utilize.

--- Silas.Web/Clients/TrueDataClient.cs
+++ Silas.Web/Clients/TrueDataClient.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Policy;
 using System.Web;
+using Silas.Domain;
 
 namespace Silas.Web.Clients
 {
-    public class TrueDataClient
+    public class TrueDataClient : IDataClient
     {
+        public IEnumerable<DataEntry> GetData()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:8080/");
+
+            HttpResponseMessage resp = client.GetAsync("api/truedata").Result;
+            resp.EnsureSuccessStatusCode();
+
+            return resp.Content.ReadAsAsync<IEnumerable<DataEntry>>().Result;
+        }
     }
 }

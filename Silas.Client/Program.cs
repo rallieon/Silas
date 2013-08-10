3d6579c11e0f02966a67578b544d939fd92b4c60
Keith La Force <klaforce@skookum.com>
2013-08-10 08:58:44 -0400

    Test client side signalR working

--- Silas.Client/Program.cs
+++ Silas.Client/Program.cs
@@ -6,6 +6,8 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
 using CsvHelper;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
 using Silas.Forecast.Models;
 
 namespace Silas.Client
@@ -14,26 +16,20 @@ namespace Silas.Client
     {
         private static void Main(string[] args)
         {
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost:8080/");
+            Initialize();
+            
+        }
 
-            // Add an Accept header for JSON format.
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            //run an api call to the web api layer every second to create stream of hits
-            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
-                                           ConfigurationSettings.AppSettings["FeedDataPath"]);
-            var csv = new CsvReader(new StreamReader(filePath));
-            csv.Configuration.ClassMapping<DataEntryMap, DataEntry>();
-
-            IEnumerable<DataEntry> entryList = csv.GetRecords<DataEntry>();
-
-            foreach (DataEntry entry in entryList)
-            {
-                HttpResponseMessage response = client.PostAsJsonAsync("api/livedata", entry).Result;
-                Console.WriteLine("{0}", (int) response.StatusCode);
-                Thread.Sleep(100);
-            }
+        private static void Initialize()
+        {
+            var hubConnection = new HubConnection("http://localhost:8080/");
+            hubConnection.TraceLevel = TraceLevels.All;
+            hubConnection.TraceWriter = Console.Out;
+            IHubProxy proxy = hubConnection.CreateHubProxy("MyHub");
+            hubConnection.Start().Wait();
+            proxy.Invoke<string>("Send", "Keith", "Hello1!").Wait();
+            proxy.Invoke<string>("Send", "Keith", "Hello2!").Wait();
+            proxy.Invoke<string>("Send", "Keith", "Hello3!").Wait();
         }
     }
 }
--- Silas.Server/Program.cs
+++ Silas.Server/Program.cs
@@ -5,6 +5,9 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Routing;
 using System.Web.Http.SelfHost;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Hosting;
+using Owin;
 using Silas.Server.DB;
 
 namespace Silas.Server
@@ -13,28 +16,38 @@ namespace Silas.Server
     {
         private static void Main(string[] args)
         {
-            var config = new HttpSelfHostConfiguration("http://localhost:8080");
+            // This will *ONLY* bind to localhost, if you want to bind to all addresses
+            // use http://*:8080 to bind to all addresses. 
+            // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
+            // for more information.
+            string url = "http://localhost:8080";
 
-            //setup data directory
-            AppDomain.CurrentDomain.SetData("DataDirectory", Directory.GetCurrentDirectory());
-
-            Database.SetInitializer(new LiveDataContextInitializer());
-            using (var context = new LiveDataContext())
+            using (WebApp.Start<Startup>(url))
             {
-                context.Database.Initialize(true);
-            }
-
-            config.Routes.MapHttpRoute(
-                name: "API Default",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new {id = RouteParameter.Optional}
-                );
-
-            using (var server = new HttpSelfHostServer(config))
-            {
-                server.OpenAsync().Wait();
-                Console.WriteLine("Press Enter to quit.");
+                Console.WriteLine("Server running on {0}", url);
                 Console.ReadLine();
+            }
+        }
+
+        class Startup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                // Turn cross domain on 
+                var config = new HubConfiguration { EnableCrossDomain = true, EnableDetailedErrors = true};
+
+                // This will map out to http://localhost:8080/signalr by default
+                app.MapHubs(config);
+            }
+        }
+
+        public class MyHub : Hub
+        {
+            public string Send(string name, string message)
+            {
+                Console.WriteLine("Name = " + name + ":Message=" + message);
+                Clients.All.addMessage(name, message);
+                return message;
             }
         }
     }

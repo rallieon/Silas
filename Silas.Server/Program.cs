db371ca8bbf049ceb6dddf7b4a36c567470a28f9
Keith La Force <klaforce@skookum.com>
2013-08-11 16:21:29 -0400

    Breaking out Deck and doing some cleanup.

--- Silas.Server/Program.cs
+++ Silas.Server/Program.cs
@@ -1,25 +1,15 @@
 ﻿using System;
-using System.Data.Entity;
-using System.IO;
-using System.Net.Http;
 using System.Web.Http;
-using System.Web.Http.Routing;
-using System.Web.Http.SelfHost;
 using Microsoft.AspNet.SignalR;
 using Microsoft.Owin.Hosting;
 using Owin;
-using Silas.Server.DB;
 
 namespace Silas.Server
 {
-    internal class Program
+    class Program
     {
         private static void Main(string[] args)
         {
-            // This will *ONLY* bind to localhost, if you want to bind to all addresses
-            // use http://*:8080 to bind to all addresses. 
-            // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
-            // for more information.
             string url = "http://localhost:8080";
 
             using (WebApp.Start<Startup>(url))
@@ -33,21 +23,18 @@ namespace Silas.Server
         {
             public void Configuration(IAppBuilder app)
             {
-                // Turn cross domain on 
-                var config = new HubConfiguration { EnableCrossDomain = true, EnableDetailedErrors = true};
+                var signalRConfig = new HubConfiguration { EnableCrossDomain = true, EnableDetailedErrors = true};
+                app.MapHubs(signalRConfig);
 
-                // This will map out to http://localhost:8080/signalr by default
-                app.MapHubs(config);
-            }
-        }
+                // Configure Web API for self-host. 
+                HttpConfiguration webAPIConfig = new HttpConfiguration();
+                webAPIConfig.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
 
-        public class MyHub : Hub
-        {
-            public string Send(string name, string message)
-            {
-                Console.WriteLine("Name = " + name + ":Message=" + message);
-                Clients.All.addMessage(name, message);
-                return message;
+                app.UseWebApi(webAPIConfig); 
             }
         }
     }
